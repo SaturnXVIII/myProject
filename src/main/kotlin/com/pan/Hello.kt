d1f1871d733438603f98f7b3af2c1a025daf247b
PAN <c.y.pan@outlook.com>
2020-04-05 20:14:46 +0800

    Initial commit

---
+++ src/main/kotlin/com/pan/Hello.kt
@@ -0,0 +1,21 @@
+package com.pan
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+
+//    物件
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
+
+//fun main(args: Array<String>) {
+//
+//}
+
